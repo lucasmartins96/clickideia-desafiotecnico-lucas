31505e43c057afd92bd21362638d13762d66cb5a
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2022-12-15 20:33:13 -0300

    (back-end): add db config

--- back-end/src/config/index.ts
+++ back-end/src/config/index.ts
@@ -8,3 +8,11 @@ export const authConfig = {
   password: String(process.env.PASSWORD),
   expires: '1d',
 };
+
+export const dbConfig = {
+  name: String(process.env.DB_NAME),
+  password: String(process.env.DB_PASSWORD),
+  port: String(process.env.DB_PORT),
+  user: String(process.env.DB_USER),
+  host: String(process.env.DB_HOST),
+};
