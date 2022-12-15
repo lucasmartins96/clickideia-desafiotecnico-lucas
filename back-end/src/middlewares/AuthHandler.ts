df161b2df4d27d5e94b1f4e2d2ffcf3eabb368e9
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2022-12-14 22:01:27 -0300

    (back-end): add auth middleware

---
+++ back-end/src/middlewares/AuthHandler.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+
+class AuthHandler {
+  public static handle(req: Request, res: Response, next: NextFunction) {
+    let token = req.headers.authorization;
+
+    try {
+      if (!token || !token.includes('Bearer')) {
+        return res.status(401).json({ message: 'Access denied' });
+      }
+
+      token = token.replace('Bearer ', '');
+
+      const verified = verify(token, 'SECRET');
+
+      if (!verified)
+        return res
+          .status(401)
+          .json({ message: 'Token verification failed, authorization denied!' });
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export default AuthHandler;
