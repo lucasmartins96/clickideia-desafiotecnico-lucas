2318e16d3d51af6cda454c1885cd656fb11e6689
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2022-12-14 22:02:02 -0300

    (back-end): use auth route

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import ErrorHandler from './middlewares/ErrorHandler';
+import authRoute from './routes/Auth';
 import routes from './routes/Routes';
 
 const app = express();
 app.use(express.json());
+app.use(authRoute);
 app.use(routes);
 app.use(ErrorHandler.handle);
 
