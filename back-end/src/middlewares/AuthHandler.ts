08c26f180a26b0cad96f1286b9891dae88543372
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2022-12-15 08:16:13 -0300

    (back-end): get secret from authConfig

--- back-end/src/middlewares/AuthHandler.ts
+++ back-end/src/middlewares/AuthHandler.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
+import { authConfig } from '../config';
 
 class AuthHandler {
   public static handle(req: Request, res: Response, next: NextFunction) {
@@ -12,7 +13,7 @@ class AuthHandler {
 
       token = token.replace('Bearer ', '');
 
-      const verified = verify(token, 'SECRET');
+      const verified = verify(token, authConfig.secret);
 
       if (!verified)
         return res
