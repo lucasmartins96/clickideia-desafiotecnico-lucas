710929986eebabc727ada0d58d7862f81ad6c3ca
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2022-12-17 08:27:57 -0300

    (front-end): create react app initial files

---
+++ front-end/src/App.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import ProTip from './ProTip';
+import {
+  CssBaseline,
+  FormControl,
+  FormHelperText,
+  Input,
+  InputLabel,
+  TextField,
+} from '@mui/material';
+import { Stack } from '@mui/system';
+import { NewTodoForm } from './components/NewTodoForm';
+import { TodosDashboard } from './components/TodosDashboard';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}.
+    </Typography>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <CssBaseline />
+      <NewTodoForm />
+      <TodosDashboard />
+    </>
+  );
+}
