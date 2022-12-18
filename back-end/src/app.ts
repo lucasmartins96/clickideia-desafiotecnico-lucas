37c46b498ba43ae9eae16654101bf4bc0f305ee3
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2022-12-18 16:32:00 -0300

    (back-end): add middleware cors

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import ErrorHandler from './middlewares/ErrorHandler';
 import routes from './routes/Routes';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(ErrorHandler.handle);
