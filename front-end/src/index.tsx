ddb95e2aa1049426cd2861b22b169823f0b91fd3
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2022-12-18 16:25:18 -0300

    (front-end): wrap app with provider

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -2,12 +2,15 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
+import { TodoProvider } from './context/todoProvider';
 
 ReactDOM.render(
   <React.Fragment>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
-    <App />
+    <TodoProvider>
+      <App />
+    </TodoProvider>
   </React.Fragment>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
